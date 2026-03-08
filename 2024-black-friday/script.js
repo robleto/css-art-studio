744b2862078dae22e167be9af37c3f6d0e881459
Greg Robleto <greg@robleto.com>
2026-03-08 18:10:45 -0400

    Museum gallery redesign: 2024-2025 artworks, frame polish, subtitle style fix

---
+++ 2024-black-friday/script.js
@@ -0,0 +1,5 @@
+/* It's beginning to look a lot like the busiest in-store shopping day of the year. Happy Black Friday y'all, I hope you find the gifts you are seeking and fill up your cart just like this.
+
+
+More CSS Art from Greg Robleto at
+cssartstudio.com  */ 
